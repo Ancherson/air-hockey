4c5a11a9c7496f42cb327d51fa944805e4fc988d
Jules Cherion <julescherion@yahoo.fr>
2021-02-12 18:44:21 +0100

    Client prototype

--- prototype/src/main/java/air/hockey/prototype/udp/ProtoClient.java
+++ prototype/src/main/java/air/hockey/prototype/udp/ProtoClient.java
@@ -1,4 +1,59 @@
 package air.hockey.prototype.udp;
 
-public class ProtoClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+
+    class MessageSender  implements Runnable{
+
+        public final static int PORT = 6631;
+        private String hostname = "localhost";
+        private DatagramSocket socket;
+        MessageSender(DatagramSocket s) throws InterruptedException {
+            socket = s;
+        }
+
+
+        public void Message(String s) throws IOException {
+            byte byteMessage[] = s.getBytes();
+            InetAddress address = InetAddress.getByName(hostname);
+            DatagramPacket msg = new DatagramPacket(byteMessage, byteMessage.length, address, PORT);
+            socket.send(msg);
+        }
+        public void run(){
+            boolean co = false;
+            do{
+                try{
+                    Message("y a quelqu'un ?");
+                    co=true;
+                } catch (IOException e) {
+
+                }
+
+            } while (!co);
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true){
+            try{
+                while(!in.ready()){
+                    Thread.sleep(100);
+                }
+            Message(in.readLine());
+        }catch(Exception e){
+                System.out.println("erreur");
+            }
+        }
+        }
+    }
+    public class ProtoClient{
+
+            public static void main(String[] args) throws InterruptedException, SocketException {
+                DatagramSocket socket = new DatagramSocket();
+                MessageSender s = new MessageSender(socket);
+                Thread rt = new Thread(s);
+                rt.start();
+    }
 }
+
