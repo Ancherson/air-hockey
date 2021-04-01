fce5ab8b91cbe3a6a95f4b2092d96c63ba0b9394
Etienne Nedjai <etienne.nedjai@laposte.net>
2021-04-01 15:29:35 +0200

    Added ending screen

--- Projet/gui/src/main/java/airhockey/gui/EndMenu.java
+++ Projet/gui/src/main/java/airhockey/gui/EndMenu.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
+import static javafx.scene.paint.Color.WHITE;
+import static javafx.scene.paint.Color.BLACK;
+
 public class EndMenu extends BorderPane{
     
     private Button back;
@@ -17,12 +20,34 @@ public class EndMenu extends BorderPane{
         this.menu = menu;
         this.won = won;
 
-        back = new Button("back");
+        this.setWidth(800);
+        this.setHeight(500);
+        this.setStyle("-fx-background-color: #282828;");
+
         message = new Label();
+        message.setMinHeight(100);
+        message.setMaxHeight(100);        
+        message.setStyle("-fx-font : 28 Ubuntu;");
+        message.setTextFill(WHITE);
+
+        back = new Button("back");
+ 
 
         back.setMaxSize(100, 60);
         back.setPrefSize(60, 50);
+        back.setStyle("-fx-background-color: #565656;");
+        back.setTextFill(WHITE);
 
+        back.setOnMouseEntered((action)->{
+            back.setStyle("-fx-background-color: #FFFFFF;");
+            back.setTextFill(BLACK);
+        });
+
+        back.setOnMouseExited((action)->{
+            back.setStyle("-fx-background-color: #565656;");
+            back.setTextFill(WHITE);
+        });
+        
         back.setOnAction(value->{
             menu.setScene(1);
         });
@@ -33,8 +58,8 @@ public class EndMenu extends BorderPane{
             message.setText("Vous avez perdu au jeu! T'es nul");
         }
 
-        this.setTop(back);
-        this.setAlignment(back, Pos.TOP_LEFT);
+        this.setBottom(back);
+        this.setAlignment(back, Pos.BOTTOM_RIGHT);
 
         this.setCenter(message);
         this.setAlignment(message, Pos.CENTER);
