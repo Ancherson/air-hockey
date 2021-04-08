b74d4e6f333a8e46622a56aefb36a796c912e7ae
Etienne Nedjai <etienne.nedjai@laposte.net>
2021-04-08 11:22:29 +0200

    Added class sound

---
+++ Projet/gui/src/main/java/airhockey/gui/Sound.java
@@ -0,0 +1,54 @@
+package airhockey.gui;
+
+import java.io.File;
+import java.util.*;
+
+import javafx.scene.media.*;
+import javafx.util.Duration;
+
+
+public class Sound {
+    private HashMap<String,MediaPlayer> playlist;
+    private String path;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    public Sound(){
+        playlist = new HashMap<>();
+
+        path = "../ressources/bruit-goutte-deau.wav"; 
+        media = new Media(new File(path).toURI().toString());           
+        mediaPlayer = new MediaPlayer(media);        
+        playlist.put("collisionRelax", mediaPlayer);
+
+        path = "../ressources/thunder.wav";
+        media = new Media(new File(path).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        playlist.put("shakingRelax",mediaPlayer);
+
+        path = "../ressources/ambiance_relax.wav";
+        media = new Media(new File(path).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        playlist.put("ambianceRelax",mediaPlayer);
+
+    }
+
+    public void play(String name){
+        playlist.get(name).play();
+    }
+
+    public void repeat(String name){
+        playlist.get(name).setOnEndOfMedia(new Runnable(){
+            public void run(){
+                playlist.get(name).seek(Duration.ZERO);
+            }
+        });
+        playlist.get(name).play();
+
+    }
+
+    public void stop(String name){
+        playlist.get(name).stop();
+    }
+    
+}
