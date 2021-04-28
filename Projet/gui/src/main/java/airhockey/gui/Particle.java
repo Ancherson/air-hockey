52969a6d3e57f73951d39d953af482f19f241d05
Alexandre LEYMARIE <alexandre.j.leymarie@gmail.com>
2021-04-28 19:07:39 +0200

    Added javadoc for Particle.java and ParticleManager.java (and fixed an error in the code of ParticleManager.java)

--- Projet/gui/src/main/java/airhockey/gui/Particle.java
+++ Projet/gui/src/main/java/airhockey/gui/Particle.java
@@ -3,11 +3,31 @@ package airhockey.gui;
 import airhockey.model.Vector;
 import airhockey.model.Circle;
 
+/**
+ * This class represents a particle, used in visual effects for collisions and explosions
+ */
 public class Particle extends Circle{
+    /**
+     * The speed of the particle
+     */
     private Vector speed;
+    /**
+     * The amount of time the particle will live in total (in seconds).
+     * It is used for calculating the opacity of the particle
+     */
     private double startLife;
+    /**
+     * The amount of time the particle has left to live (in seconds)
+     */
     private double life;
 
+    /**
+     * The constructor of the particle
+     * @param p the particle's position
+     * @param s the particle's speed
+     * @param r the particle's radius
+     * @param l the amount of time the particle will live (in seconds)
+     */
     public Particle(Vector p, Vector s, double r, double l){
         super(p, r);
         speed = s;
@@ -23,6 +43,10 @@ public class Particle extends Circle{
         return life;
     }
 
+    /**
+     * Updates the particle's position and life based on the time elapsed since last frame
+     * @param dt the time elapsed since last frame
+     */
     public void update(double dt){
         position = position.add(speed.multiply(dt));
         life -= dt;
