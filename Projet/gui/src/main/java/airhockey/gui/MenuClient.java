83c0bb242061ed8b68abd2cfc318339e6cfe5f9c
Jules Cherion <julescherion@yahoo.fr>
2021-05-06 16:13:41 +0200

    Javadoc of MenuClient

--- Projet/gui/src/main/java/airhockey/gui/MenuClient.java
+++ Projet/gui/src/main/java/airhockey/gui/MenuClient.java
@@ -18,6 +18,11 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+
+/**
+ * This Class is the main class of the GUI.
+ * It contains all the different pages and multiple function of the GUI
+ */
 public class MenuClient extends Application {
     private int WIDTH = 800;
     private int HEIGHT = 500;
@@ -43,6 +48,11 @@ public class MenuClient extends Application {
     private CreateMenu create;
     private PublicWait waiting;
 
+    /**
+     * Function equivalent to the constructor. It launches the Javafx Thread
+     * @param primaryStage The stage that will contain every elements of the GUI
+     * @throws Exception an exception is thrown because when you launch a Thread you can have multiples different errors
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
@@ -85,6 +95,10 @@ public class MenuClient extends Application {
         });
     }
 
+    /**
+     * Function that swaps to the different pages of the GUI
+     * @param S is the String that refers to the name of the page, for example "first" for the first page
+     */
     public void setScene(String S) {
         switch (S) {
             case "first":
@@ -154,28 +168,50 @@ public class MenuClient extends Application {
         }
     }
 
+    /**
+     * Function that returns the model of the game used by the GUI
+     * @return a model
+     */
     public Model getModel(){
         return model;
     }
 
+    /**
+     * Function to mute the background sound
+     */
     public void pauseSound(){
         sound.pause("ambianceRelax");
     }
 
+    /**
+     * Function to loop the background sound
+     */
     public void repeatSound(){
         sound.reload("ambianceRelax");
         sound.repeat("ambianceRelax");
     }
 
+    /**
+     * Function that sets the isPaused boolean that refers to is the background sound is played or not
+     * @param b is the boolean value you give to isPaused
+     */
     public void setIsPaused(boolean b){
         isPaused = b;
     }
 
+    /**
+     * Function that gives the isPaused boolean that refers to is the background sound is played or not
+     * @return the boolean value of isPaused
+     */
     public boolean getIsPaused(){
         return isPaused;
     }
 
-
+    /**
+     * Function that will swap the vision of the game and lanch the game
+     * @param numplayer
+     * @param training
+     */
     public void setView(int numplayer, boolean training) {
         view = new View(this, model, numplayer, training);
         game = new Scene(view);
@@ -184,6 +220,9 @@ public class MenuClient extends Application {
         setScene("game");
     }
 
+    /**
+     * Function that will ask the creation of a room to the server by creating a Client and that swap to the creation page
+     */
     public void createRoom() {
         try {
             client = new Client(model,Platform::runLater,create::setID,waiting::connected,this::lostConnexion);
@@ -196,6 +235,11 @@ public class MenuClient extends Application {
         }
     }
 
+    /**
+     * Function that will ask to join the room that correspond to the id to the server by creating a Client
+     * and that will swap to the join page
+     * @param id is the String that le player typed referring to the id of the room that he wants to join
+     */
     public void joinRoom(String id) {
         try {
             client = new Client(model, Platform::runLater, create::setID,waiting::connected,this::lostConnexion, joinMenu::setMessage);
@@ -209,6 +253,9 @@ public class MenuClient extends Application {
         }
     }
 
+    /**
+     * Function that create a Client then launch the Function joinRoomPublic that create a public room or join one
+     */
     public void joinPublicRoom() {
         try {
             client = new Client(model,Platform::runLater,create::setID,waiting::connected,this::lostConnexion);
@@ -224,6 +271,10 @@ public class MenuClient extends Application {
         }
     }
 
+    /**
+     * Function that return the boolean isFinished of Client
+     * @return the boolean isFinished of Client
+     */
     public boolean isFinished() {
         return client.isFinished();
     }
@@ -235,10 +286,16 @@ public class MenuClient extends Application {
         setScene("last");
     }
 
+    /**
+     * Function that launch the function lostConnexion of View
+     */
     public void lostConnexion() {
         view.lostConnexion();
     }
 
+    /**
+     * Function that close the client
+     */
     public void closeClient()  {
         if(client!= null){
             try {
@@ -249,11 +306,18 @@ public class MenuClient extends Application {
         }
     }
 
+    /**
+     * Function that close the running game
+     */
     public void close() {
         model = new Model();
         close(null);
     }
 
+    /**
+     * Function that close the Thread animation of View
+     * @param windowEvent Event that should be the close of the window or null if called by close()
+     */
     public void close(WindowEvent windowEvent) {
         if(view != null) {
             view.close();
